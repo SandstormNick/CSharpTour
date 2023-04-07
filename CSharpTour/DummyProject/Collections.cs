6abfd93ecfeb554620824dfe411384f25905eed5
SandstormNick <gibbensnick@gmail.com>
2023-04-07 11:52:50 +0200

    Finished Introduction section. Added some changes to the VS sln

---
+++ CSharpTour/DummyProject/Collections.cs
@@ -0,0 +1,41 @@
+﻿//Program structure example
+
+//The fully qualified name of this class is Acme.Collections.Stack.
+//The class contains several members: a field named _top, two methods named Push and Pop, and a nested class named Entry.
+//The Entry class further contains three members: a property named Next, a property named Data, and a constructor.
+//The Stack is a generic class. It has one type parameter, T that is replaced with a concrete type when it's used.
+
+namespace Acme.Collections;
+public class Stack<T>
+{
+    Entry _top;
+
+    public void Push(T data)
+    {
+        _top = new Entry(_top, data);
+    }
+
+    public T Pop()
+    {
+        if (_top == null)
+        {
+            throw new InvalidOperationException();
+        }
+        T result = _top.Data;
+        _top = _top.Next;
+
+        return result;
+    }
+
+    class Entry
+    {
+        public Entry Next { get; set; }
+        public T Data { get; set; }
+
+        public Entry(Entry next, T data)
+        {
+            Next = next;
+            Data = data;
+        }
+    }
+}
--- CSharpTour/DummyProject/Program.cs
+++ CSharpTour/DummyProject/Program.cs
@@ -1,6 +1,18 @@
-﻿string s = "Hello World";
+﻿using acmeCol = Acme.Collections;
 
-Console.WriteLine(s);
+//var s = new Acme.Collections.Stack<int>();
+var s = new acmeCol.Stack<int>();
+s.Push(1); // stack contains 1
+s.Push(10); // stack contains 1, 10
+s.Push(100); // stack contains 1, 10, 100
+Console.WriteLine(s.Pop()); // stack contains 1, 10
+Console.WriteLine(s.Pop()); // stack contains 1
+Console.WriteLine(s.Pop()); // stack is empty
+
+
+string s1 = "Hello World";
+
+Console.WriteLine(s1);
 
 //An identifier is a variable name.
 //An identifier may be a C# reserved word, if it's prefixed by @.
@@ -8,3 +20,37 @@ Console.WriteLine(s);
 string @string = "string is a reserved word in c#";
 
 Console.WriteLine(@string);
+
+//In the following example, an int value is converted to object and back again to int.
+int i = 123;
+object o = i;    // Boxing
+int j = (int)o;  // Unboxing
+Console.WriteLine(j);
+
+//this boxing/unboxing would break the program though
+string s2 = "string";
+object o2 = s2;
+int r = (int)o2;
+Console.WriteLine(r);
+
+
+class IAmAClass : IAmAnInterface
+{
+    public void IAmAMethod()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+struct IAmAStruct : IAmAnInterface
+{
+    public void IAmAMethod()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+interface IAmAnInterface
+{
+    public void IAmAMethod();
+}
