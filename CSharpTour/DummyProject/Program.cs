bf5f205dcf0dc5ddb39968dc014633aac1c6eef8
SandstormNick <gibbensnick@gmail.com>
2023-04-07 10:43:20 +0200

    Added VS sln to work on and play around with

---
+++ CSharpTour/DummyProject/Program.cs
@@ -0,0 +1,10 @@
+﻿string s = "Hello World";
+
+Console.WriteLine(s);
+
+//An identifier is a variable name.
+//An identifier may be a C# reserved word, if it's prefixed by @.
+//Using a reserved word as an identifier can be useful when interacting with other languages.
+string @string = "string is a reserved word in c#";
+
+Console.WriteLine(@string);
