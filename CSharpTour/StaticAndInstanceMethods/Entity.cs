59a6204eabd37c7af5f8581d88d692a9bd782a41
SandstormNick <gibbensnick@gmail.com>
2023-04-13 18:16:48 +0200

    Static and Instance methods

---
+++ CSharpTour/StaticAndInstanceMethods/Entity.cs
@@ -0,0 +1,25 @@
+﻿class Entity
+{
+    static int s_nextSerialNo;
+    int _serialNo;
+
+    public Entity()
+    {
+        _serialNo = s_nextSerialNo++;
+    }
+
+    public int GetSerialNo()
+    {
+        return _serialNo;
+    }
+
+    public static int GetNextSerialNo()
+    {
+        return s_nextSerialNo;
+    }
+
+    public static void SetNextSerialNo(int value)
+    {
+        s_nextSerialNo = value;
+    }
+}
---
+++ CSharpTour/StaticAndInstanceMethods/Program.cs
@@ -0,0 +1,6 @@
+﻿Entity.SetNextSerialNo(1000);
+Entity e1 = new();
+Entity e2 = new();
+Console.WriteLine(e1.GetSerialNo());          // Outputs "1000"
+Console.WriteLine(e2.GetSerialNo());          // Outputs "1001"
+Console.WriteLine(Entity.GetNextSerialNo());  // Outputs "1002"
